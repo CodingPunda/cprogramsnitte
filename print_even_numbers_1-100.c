099f9f653c7c1b1b190e2d5a93aca4ec84c670a8
Ocsid <nayak22h@hotmail.com>
2014-09-07 18:05:20 +0530

    Create print_even_numbers_1-100.c

---
+++ print_even_numbers_1-100.c
@@ -0,0 +1,15 @@
+/*This is a program to display all even numbers from one to hundred on one single line*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i=1;i<=100;i++)
+    {
+        if(i%2==0)
+            printf("%d\n",i);
+    }
+    return 0;
+}
